a072a1d0ab228050a4d5b0620f42971cb137d5f5
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2025-10-08 23:29:36 +0530

    Init Commit

---
+++ src/boxy_py/constructs.py
@@ -0,0 +1,25 @@
+class BoxPad:
+    def __init__(self, top: int, left: int, down: int, right: int):
+        self.top: int = top
+        self.left: int = left
+        self.down: int = down
+        self.right: int = right
+
+    def uniform(self, pad: int) -> "BoxPad":
+        return BoxPad(pad, pad, pad, pad)
+
+
+def get_edges(boxtype: str | None = None):
+    match boxtype:
+        case "square":
+            return ["┌", "┐", "└", "┘"]
+        case "rounded":
+            return ["╭", "╮", "╰", "╯"]
+        case "double":
+            return ["╔", "╗", "╚", "╝"]
+        case "bold":
+            return ["┏", "┓", "┗", "┛"]
+        case "empty":
+            return [" ", " ", " ", " "]
+        case _:
+            raise ValueError(f"Invalid box type: {boxtype}")
